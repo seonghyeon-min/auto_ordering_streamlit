251f4df1a29ecbfcda22c64beee91cdcc95544cf
민성현/연구원/IT SW플랫폼개발Project(seonghyeon.min@lge.com) <seonghyeon.min@lge.com>
2024-07-26 09:49:17 +0900

    option changed

--- orderingHandler.py
+++ orderingHandler.py
@@ -94,11 +94,10 @@ def SendKeyEvent(contribute, path) :
 def get_driver() :
     global driver
     
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = webdriver.Options()
     
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
 
