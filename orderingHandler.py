43a5788f21ce133e60e3e41c3d85b349f677a9db
민성현/연구원/IT SW플랫폼개발Project(seonghyeon.min@lge.com) <seonghyeon.min@lge.com>
2024-07-26 09:51:15 +0900

    push

--- orderingHandler.py
+++ orderingHandler.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
