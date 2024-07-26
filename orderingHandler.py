f7bd61b704bb404453f5f753b44bca04ef9cf375
민성현/연구원/IT SW플랫폼개발Project(seonghyeon.min@lge.com) <seonghyeon.min@lge.com>
2024-07-26 09:41:41 +0900

    option added

--- orderingHandler.py
+++ orderingHandler.py
@@ -95,8 +95,10 @@ def get_driver() :
     global driver
     
     chrome_options = webdriver.ChromeOptions()
+    
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--disable-dev-shm-usage")
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
 
